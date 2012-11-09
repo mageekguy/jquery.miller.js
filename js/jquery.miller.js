3ba5e6df5a9cfc0b7f2b5da05399bc3b59969039
Frédéric Hardy <frederic.hardy@mageekbox.net>
2012-11-09 14:26:09 +0100

    Optimization.

--- js/jquery.miller.js
+++ js/jquery.miller.js
@@ -22,18 +22,6 @@
 			.appendTo(this)
 		;
 
-		var getColumnsWidth = function() {
-				var width = 0;
-
-				columns.children().each(function() {
-						width += $(this).width();
-					}
-				);
-
-				return width;
-			}
-		;
-
 		var removeNextColumns = function() {
 				var line = $(this);
 
@@ -88,99 +76,106 @@
 		var buildColumn = function(lines) {
 				if (lines == null) {
 					$('li.parentLoading').remove();
-				} else if (lines.length <= 0) {
-					var line = $('li.parentLoading')
-						.removeClass('parent')
-						.addClass('selected')
-					;
-
-					var width = getColumnsWidth();
-
-					var panel = $('<ul>')
-						.css({ 'top': 0, 'left': width })
-						.addClass('panel')
-					;
-
-					id = line.data('id');
-
-					$.each(settings['panel']['options'], function(key, callbackGenerator) {
-							var option = $('<li>', { text: key })
-								.on('click', callbackGenerator(id))
-							;
-
-							option.appendTo(panel);
-						}
-					);
-
-					columns
-						.append(panel)
-						.scrollLeft(width + panel.width())
-					;
 				} else {
-					$('li.parentLoading').addClass('parentSelected');
+					var width = 0;
 
-					var width = getColumnsWidth();
+					var lastGrip = columns.children('div.grip:last')[0];
 
-					var column = $('<ul>')
-						.css({ 'top': 0, 'left': width })
-					;
+					if (lastGrip) {
+						lastGrip = $(lastGrip);
+						width = lastGrip.position().left + lastGrip.width();
+					}
 
-					$.each(lines, function(id, data) {
-							var line = $('<li>', { text: data['name'] })
-								.data('id', data['id'])
-								.on('click', removeNextColumns)
-								.on('click', getLines)
-								.appendTo(column)
-							;
+					if (lines.length <= 0) {
+						var line = $('li.parentLoading')
+							.removeClass('parent')
+							.addClass('selected')
+						;
 
-							if (data['parent']) {
-								line.addClass('parent');
+						var panel = $('<ul>')
+							.css({ 'top': 0, 'left': width })
+							.addClass('panel')
+						;
+
+						id = line.data('id');
+
+						$.each(settings['panel']['options'], function(key, callbackGenerator) {
+								var option = $('<li>', { text: key })
+									.on('click', callbackGenerator(id))
+								;
+
+								option.appendTo(panel);
 							}
-						}
-					);
+						);
 
-					columns
-						.append(column)
-						.scrollLeft(width += column.width())
-						.append(
-							$('<div>', { class: 'grip' })
-								.css({ 'top': 0, 'left': width })
-								.mousedown(function(event) {
-										var x = event.pageX;
-										var cursor = columns.css('cursor');
+						columns
+							.append(panel)
+							.scrollLeft(width + panel.width())
+						;
+					} else {
+						$('li.parentLoading').addClass('parentSelected');
 
-										columns
-											.css('cursor', 'col-resize')
-											.mousemove(function(event) {
-													var delta = event.pageX - x;
-													var newWidth = column.width() + delta;
+						var column = $('<ul>')
+							.css({ 'top': 0, 'left': width })
+						;
 
-													if (newWidth > settings['minWidth']) {
-														column
-															.width(newWidth)
-															.nextAll()
-																.each(function() {
-																		$(this).css('left', $(this).position().left + delta + columns.scrollLeft());
-																	}
-																)
+						$.each(lines, function(id, data) {
+								var line = $('<li>', { text: data['name'] })
+									.data('id', data['id'])
+									.on('click', removeNextColumns)
+									.on('click', getLines)
+									.appendTo(column)
+								;
+
+								if (data['parent']) {
+									line.addClass('parent');
+								}
+							}
+						);
+
+						columns
+							.append(column)
+							.scrollLeft(width += column.width())
+							.append(
+								$('<div>', { class: 'grip' })
+									.css({ 'top': 0, 'left': width })
+									.mousedown(function(event) {
+											var x = event.pageX;
+											var cursor = columns.css('cursor');
+
+											columns
+												.css('cursor', 'col-resize')
+												.mousemove(function(event) {
+														var delta = event.pageX - x;
+														var newWidth = column.width() + delta;
+
+														if (newWidth > settings['minWidth']) {
+															column
+																.width(newWidth)
+																.nextAll()
+																	.each(function() {
+																			$(this).css('left', $(this).position().left + delta + columns.scrollLeft());
+																		}
+																	)
+															;
+														}
+
+														x = event.pageX;
+													}
+												)
+												.mouseup(function() {
+														columns
+															.off('mousemove')
+															.css('cursor', cursor)
 														;
 													}
-
-													x = event.pageX;
-												}
-											)
-											.mouseup(function() {
-													columns
-														.off('mousemove')
-														.css('cursor', cursor)
-													;
-												}
-											)
-										;
-									}
-								)
-						)
-					;
+												)
+											;
+										}
+									)
+							)
+						;
+					}
 				}
 			}
 		;
